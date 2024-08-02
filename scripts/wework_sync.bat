35f316a78556b3c8bc12d20a531b8284816b7a0e
zhaozhonghua <zhaozhonghua@moseeker.com>
2024-08-02 13:39:58 +0800

    fix path

--- scripts/wework_sync.bat
+++ scripts/wework_sync.bat
@@ -1,12 +1,9 @@
 @echo on
 
 set log_date=%date:~0,4%%date:~5,2%%date:~8,2%
+
 echo %date%
-
-echo conda activate ntwork >> C:\www\ntwork\logs\wework_sync_%log_date%.log
-conda activate ntwork
-
-echo %date% python "C:\www\ntwork\scripts\wework_sync.py" >> C:\www\ntwork\logs\wework_sync_%log_date%.log
-python "C:\www\ntwork\scripts\wework_sync.py" >> C:\www\ntwork\logs\wework_sync_%log_date%.log
+echo %date% C:\Program Files\miniconda\envs\ntwork\python.exe "C:\www\ntwork\scripts\wework_sync.py" >> C:\www\ntwork\logs\wework_sync_%log_date%.log
+"C:\Program Files\miniconda\envs\ntwork\python.exe" "C:\www\ntwork\scripts\wework_sync.py" >> C:\www\ntwork\logs\wework_sync_%log_date%.log
 
 echo RUN ERROR LEVEL: %ERRORLEVEL% >> C:\www\ntwork\logs\wework_sync_%log_date%.log
