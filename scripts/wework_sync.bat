762085f384005b9d8f775b9aff45e9479622d845
zhaozhonghua <zhaozhonghua@moseeker.com>
2024-08-02 13:31:53 +0800

    fix add test script

--- scripts/wework_sync.bat
+++ scripts/wework_sync.bat
@@ -1,7 +1,8 @@
 @echo on
 
+set log_date=%date:~0,4%%date:~5,2%%date:~8,2%
 echo %date%
-echo %date% python "C:\www\ntwork\scripts\wework_sync.py" > C:\www\ntwork\logs\wework_sync_%date%.log
-python "C:\www\ntwork\scripts\wework_sync.py"
+echo %date% C:\Program Files\miniconda\envs\ntwork\scripts\python.exe "C:\www\ntwork\scripts\wework_sync.py" >> C:\www\ntwork\logs\wework_sync_%log_date%.log
+C:\Program Files\miniconda\envs\ntwork\scripts\python.exe "C:\www\ntwork\scripts\wework_sync.py" >> C:\www\ntwork\logs\wework_sync_%log_date%.log
 
-echo ERRORLEVEL:%ERRORLEVEL%
+echo RUN ERROR LEVEL: %ERRORLEVEL% >> C:\www\ntwork\logs\wework_sync_%log_date%.log
