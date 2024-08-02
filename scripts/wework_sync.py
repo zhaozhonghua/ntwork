989172eabdb647543f45a32a848e248683e8d96a
zhaozhonghua <zhaozhonghua@moseeker.com>
2024-08-02 14:16:29 +0800

    fix add get romm

--- scripts/wework_sync.py
+++ scripts/wework_sync.py
@@ -3,11 +3,37 @@ import requests
 
 class WeWorkDataSync:
 
+    host = "http://localhost:8000"
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    def get_guid(self):
+        url = f"{self.host}/client/guid/list"
+        result = requests.post(url, headers=self.headers).json()
+        data = result.get("data") or {}
+        guid_list = data.get("guid_list")
+        return guid_list[0] if guid_list else ""
+
     def get_rooms(self):
-        pass
+        guid = self.get_guid()
+        if not guid:
+            print("invalid get guid")
+            raise ValueError("invalid get guid")
+        url = f"{self.host}/room/get_rooms"
+        params = {
+            "guid": guid,
+            "page_num": 1,
+            "page_size": 500
+        }
+        result = requests.post(url, json=params, headers=self.headers).json()
+        data = result.get("data") or {}
+        room_list = data.get("room_list")
+        print(room_list)
+        return room_list
 
     def sync(self):
-        pass
+        self.get_rooms()
 
 
 if __name__ == '__main__':
