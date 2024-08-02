52eb9345fdbccf61a7e096c6fc473802fa3a830d
zhaozhonghua <zhaozhonghua@moseeker.com>
2024-08-02 15:14:55 +0800

    fix set PYTHONPATH

--- scripts/wework_sync.bat
+++ scripts/wework_sync.bat
@@ -1,6 +1,7 @@
 @echo on
 
 set log_date=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%
+set PYTHONPATH=C:\www\ntwork
 
 echo %date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,8% start >> C:\www\ntwork\logs\wework_sync_%log_date%.log
 echo %date% C:\Program Files\miniconda\envs\ntwork\python.exe "C:\www\ntwork\scripts\wework_sync.py" >> C:\www\ntwork\logs\wework_sync_%log_date%.log
