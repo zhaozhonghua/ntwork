12109f0cf2d6e23505d04bca3e0a6441f5303f43
zhaozhonghua <zhaozhonghua@moseeker.com>
2024-08-02 13:22:36 +0800

    fix add log

--- scripts/wework_sync.bat
+++ scripts/wework_sync.bat
@@ -1,7 +1,7 @@
 @echo on
 
 echo %date%
-echo %date% python "C:\www\ntwork\scripts\wework_sync.py" > output.txt
+echo %date% python "C:\www\ntwork\scripts\wework_sync.py" > C:\www\ntwork\logs\wework_sync_%date%.log
 python "C:\www\ntwork\scripts\wework_sync.py"
 
 echo ERRORLEVEL:%ERRORLEVEL%
