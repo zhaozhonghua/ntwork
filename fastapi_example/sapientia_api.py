dc9b526989c0335e4024cddaf3a496d245ec5ad3
zhaozhonghua <zhaozhonghua@moseeker.com>
2024-08-05 16:54:07 +0800

    fix config

--- fastapi_example/sapientia_api.py
+++ fastapi_example/sapientia_api.py
@@ -2,20 +2,20 @@
 
 import requests
 
-from fastapi_example.setting import SAPIENTIA_APP_IDENTIFIER, SAPIENTIA_HOST
+from fastapi_example.setting import SAPIENTIA_ACCOUNT_SECRET_KEY, SAPIENTIA_HOST
 
 
 class SapientiaApi:
 
     def __init__(self):
-        self.host = "http://localhost:8000"
+        self.host = SAPIENTIA_HOST
         self.headers = {
             "Content-Type": "application/json",
-            "Authorization": f"Token {SAPIENTIA_APP_IDENTIFIER}",
+            "Authorization": f"Token {SAPIENTIA_ACCOUNT_SECRET_KEY}",
         }
 
     def sync_wework_event_data_notice(self, guid, message):
-        url = f"{SAPIENTIA_HOST}/api/app/utv/v1/sync/wework/event/data"
+        url = f"{self.host}/api/app/utv/v1/sync/wework/event/data"
         params = {
             "guid": guid,
             "message": message,
@@ -23,8 +23,25 @@ class SapientiaApi:
         result = requests.post(url, json=params, headers=self.headers).json()
         return result
 
+    def get_group_release(self, conversation_id):
+        url = f"{self.host}/api/app/utv/v1/wework/group/release"
+        params = {
+            "conversation_id": conversation_id,
+        }
+        result = requests.post(url, json=params, headers=self.headers).json()
+        data = result.get("data") or {}
+        return data.get("identifier")
+
     def bot_reply(self, content, context_id, end_user):
-        url = f"{SAPIENTIA_HOST}/api/app/utv/v1/agent/qa"
+        release_identifier = self.get_group_release(context_id)
+        if not release_identifier:
+            return
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Token {release_identifier}",
+        }
+
+        url = f"{self.host}/api/app/utv/v1/agent/qa"
         params = {
             "messages": [
                 {
@@ -33,7 +50,8 @@ class SapientiaApi:
                 }
             ],
             "context_id": context_id,
-            "end_user": end_user
+            "end_user": end_user,
+            "source": 6
         }
-        result = requests.post(url, json=params, headers=self.headers).json()
+        result = requests.post(url, json=params, headers=headers).json()
         return result.get("data", {}).get("answer", "")
