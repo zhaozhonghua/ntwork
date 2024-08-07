ffd7343a0ea814b495be4e81b73428ccd0f70982
zhaozhonghua <zhaozhonghua@moseeker.com>
2024-08-07 13:18:49 +0800

    fix add bat

--- scripts/start_api_app.bat
+++ scripts/start_api_app.bat
@@ -1,13 +1,8 @@
-@echo on
+@echo off
 
 set log_date=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%
-set PYTHONPATH=C:\www\ntwork\fastapi_example
 set logfile=C:\www\ntwork\logs\start_api_app_%log_date%.log
 
-echo %date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,8% start >> %logfile%
-echo %date% C:\Program Files\miniconda\envs\ntwork\python.exe "main.py" >> %logfile%
+set PYTHONPATH=C:\www\ntwork\fastapi_example
 cd C:\www\ntwork\fastapi_example
 "C:\Program Files\miniconda\envs\ntwork\python.exe" "main.py" >> %logfile%
-
-echo RUN ERROR LEVEL: %ERRORLEVEL% >> %logfile%
-echo %date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,8% end >> %logfile%
