1fbead02de9ee4abe50c1564a729073c2152ceb2
zhaozhonghua <zhaozhonghua@moseeker.com>
2024-08-08 10:43:00 +0800

    fix add download_cdn_file

--- fastapi_example/mgr.py
+++ fastapi_example/mgr.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 import random
 import re
+import copy
 import time
+import os
+import uuid
 from typing import Dict, Union
 
 from fastapi.logger import logger
 
 import ntwork
 from exception import ClientNotExists
-from ntwork.const import notify_type
+from ntwork.const import notify_type, send_type
 from ntwork.utils.singleton import Singleton
 from sapientia_api import SapientiaApi
 from utils import generate_guid
@@ -17,6 +20,19 @@ from setting import SAPIENTIA_ACCOUNT_SECRET_KEY, SAPIENTIA_HOST
 
 class ClientWeWork(ntwork.WeWork):
     guid: str = ""
+
+    def wx_cdn_download(self, url: str, auth_key: str, aes_key: str, size: int, save_path):
+        """
+        下载wx类型的cdn文件，以https开头
+        """
+        data = {
+            'url': url,
+            'aes_key': aes_key,
+            'auth_key': auth_key,
+            'size': size,
+            'save_path': save_path
+        }
+        return self.__send_sync(send_type.MT_WXCDN_DOWNLOAD_MSG, data)
 
 
 class ClientManager(metaclass=Singleton):
@@ -62,12 +78,74 @@ class ClientManager(metaclass=Singleton):
         if guid in self.__client_map:
             del self.__client_map[guid]
 
-    def reply(self, wework, message):
+    def download_cdn_file(self, client_wework, message, file_name):
+        data = message["data"]
+        aes_key = data["cdn"]["aes_key"]
+        file_size = data["cdn"]["size"]
+
+        # 获取当前工作目录，然后与文件名拼接得到保存路径
+        directory = os.path.join(os.getcwd(), "tmp")
+        # 如果目录不存在，则创建目录
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        save_path = os.path.join(directory, file_name)
+
+        # 下载文件到本地
+        if "url" in data["cdn"].keys() and "auth_key" in data["cdn"].keys():
+            url = data["cdn"]["url"]
+            auth_key = data["cdn"]["auth_key"]
+            # 下载wx类型的cdn文件，以https开头
+            data = {
+                'url': url,
+                'auth_key': auth_key,
+                'aes_key': aes_key,
+                'size': file_size,
+                'save_path': save_path
+            }
+            result = client_wework.wx_cdn_download(**data)
+        elif "file_id" in data["cdn"].keys():
+            if message["type"] == notify_type.MT_RECV_IMAGE_MSG:
+                file_type = 2
+            elif message["type"] == notify_type.MT_RECV_FILE_MSG:
+                file_type = 5
+            else:
+                logger.warning("not exist file_id")
+                return
+            file_id = data["cdn"]["file_id"]
+            result = client_wework.c2c_cdn_download(file_id, aes_key, file_size, file_type, save_path)
+        else:
+            logger.error(f"something is wrong, data: {data}")
+            return
+        # 输出下载结果
+        msg = f"download_cdn_file result: {result}"
+        logger.debug(msg)
+        print(msg)
+
+    def convert_message(self, wework, message):
+        c_message = copy.deepcopy(message)
+        client_wework = self.get_client(wework.guid)
+        msg_type = c_message.get("type")
+        if msg_type == notify_type.MT_RECV_IMAGE_MSG:
+            download_file_name = f"{uuid.uuid4().hex}.jpg"
+            self.download_cdn_file(client_wework, c_message, download_file_name)
+        if msg_type == notify_type.MT_RECV_VOICE_MSG:
+            pass
+        if msg_type == notify_type.MT_RECV_FILE_MSG:
+            pass
+        if msg_type == notify_type.MT_RECV_VIDEO_MSG:
+            pass
+        return c_message
+
+    def report_message(self, wework, message):
         try:
-            self.sap_api.sync_wework_event_data_notice(wework.guid, message)
+            c_message = self.convert_message(wework, message)
+            self.sap_api.sync_wework_event_data_notice(wework.guid, c_message)
         except Exception as e:
             logger.exception(e)
+
+    def reply(self, wework, message):
         receive_start_time = int(time.time())
+        self.report_message(wework, message)
         logger.info("login_user_id:%s message:%s", self.login_user_id, message)
         msg_type = message.get("type")
         data = message.get("data") or {}
--- scripts/start_api_app.bat
+++ scripts/start_api_app.bat
@@ -6,20 +6,3 @@ set logfile=C:\www\ntwork\logs\start_api_app_%log_date%.log
 set PYTHONPATH=C:\www\ntwork\fastapi_example
 cd C:\www\ntwork\fastapi_example
 "C:\Program Files\miniconda\envs\ntwork\python.exe" "main.py" >> %logfile%
-
-
-@echo off
-echo Starting Python web backend...
-cd C:\www\ntwork\fastapi_example
-start "C:\Program Files\miniconda\envs\ntwork\python.exe" "main.py"
-
-echo Waiting for web backend to start...
-timeout /t 30 /nobreak
-
-echo Starting initialization script...
-set PYTHONPATH=C:\www\ntwork
-cd C:\www\ntwork
-"C:\Program Files\miniconda\envs\ntwork\python.exe" scripts\init_wework.py
-
-set PYTHONPATH=C:\www\ntwork\fastapi_example
-echo All processes started successfully.
